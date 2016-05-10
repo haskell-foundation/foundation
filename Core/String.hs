ecb74917aa03227cf258b56c725cdc6d3e122cf8
Vincent Hanquez <vincent@snarc.org>
2016-05-10 08:20:25 +0100

    add some documentation for String

--- Core/String.hs
+++ Core/String.hs
@@ -5,7 +5,20 @@
 -- Stability   : experimental
 -- Portability : portable
 --
--- Opaque packed String encoded in UTF8
+-- Opaque packed String encoded in UTF8.
+--
+-- The type is an instance of IsString and IsList, which allow OverloadedStrings
+-- for string literal, and 'fromList' to convert a [Char] (Prelude String) to a packed
+-- representation
+--
+-- > {-# LANGUAGE OverloadedStrings #-}
+-- > s = "Hello World :: String
+--
+-- > s = fromList ("Hello World" :: Prelude.String) :: String
+--
+-- Each unicode code point is represented by a variable encoding of 1 to 4 bytes,
+--
+-- For more information about UTF8: <https://en.wikipedia.org/wiki/UTF-8>
 --
 module Core.String
     ( String
