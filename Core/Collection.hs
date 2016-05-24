f7aa8f0edad3485655b5c77479fa4885cadce5f7
Vincent Hanquez <vincent@snarc.org>
2016-05-24 06:25:01 +0100

    introduce a monomorphic Foldable typeclass

--- Core/Collection.hs
+++ Core/Collection.hs
@@ -13,6 +13,7 @@
 module Core.Collection
     ( Element
     , InnerFunctor(..)
+    , Foldable(..)
     , SemiOrderedCollection(..)
     , OrderedCollection(..)
     , MutableCollection(..)
@@ -53,6 +54,29 @@ class InnerFunctor c => SemiOrderedCollection c where
     length :: c -> Int
     -- | Create a collection with a single element
     singleton :: Element c -> c
+
+class Foldable collection where
+    -- | Left-associative fold of a structure.
+    --
+    -- In the case of lists, foldl, when applied to a binary operator, a starting value (typically the left-identity of the operator), and a list, reduces the list using the binary operator, from left to right:
+    --
+    -- > foldl f z [x1, x2, ..., xn] == (...((z `f` x1) `f` x2) `f`...) `f` xn
+    -- Note that to produce the outermost application of the operator the entire input list must be traversed. This means that foldl' will diverge if given an infinite list.
+    --
+    -- Also note that if you want an efficient left-fold, you probably want to use foldl' instead of foldl. The reason for this is that latter does not force the "inner" results (e.g. z f x1 in the above example) before applying them to the operator (e.g. to (f x2)). This results in a thunk chain O(n) elements long, which then must be evaluated from the outside-in.
+    foldl :: (a -> Element collection -> a) -> a -> collection -> a
+
+
+    -- | Left-associative fold of a structure but with strict application of the operator.
+    foldl' :: (a -> Element collection -> a) -> a -> collection -> a
+
+    -- | Right-associative fold of a structure.
+    --
+    -- > foldr f z [x1, x2, ..., xn] == x1 `f` (x2 `f` ... (xn `f` z)...)
+    foldr :: (Element collection -> a -> a) -> a -> collection -> a
+
+    -- | Right-associative fold of a structure, but with strict application of the operator.
+    foldr' :: (Element collection -> a -> a) -> a -> collection -> a
 
 -- | A set of methods for ordered colection
 class (IsList c, Item c ~ Element c, Monoid c, SemiOrderedCollection c) => OrderedCollection c where
