174206db7818d05e870e19f2f7e0a8e346ea5a2f
Vincent Hanquez <vincent@snarc.org>
2016-05-24 06:27:49 +0100

    PrimType: add addr index/read/write handling. WIP.

--- Core/Primitive/Types.hs
+++ Core/Primitive/Types.hs
@@ -51,6 +51,26 @@ class PrimType ty where
                  -> ty                                 -- ^ the new value to store
                  -> prim ()
 
+    -----
+    -- Addr# section
+    -----
+
+    -- | Read from Address, without a state. the value read should be considered a constant for all
+    -- pratical purpose, otherwise bad thing will happens.
+    primAddrIndex :: Addr# -> Int -> ty
+
+    -- | Read a value from Addr in a specific primitive monad
+    primAddrRead :: PrimMonad prim
+                 => Addr#
+                 -> Int -- TODO: byte or index ?
+                 -> prim ty
+    -- | Write a value to Addr in a specific primitive monad
+    primAddrWrite :: PrimMonad prim
+                  => Addr#
+                  -> Int -- TODO: byte or index ?
+                  -> ty
+                  -> prim ()
+
 -- return the index and mask to a bit in a bitmap
 bitmapAddr :: Int# -> (# Int# , Word# #)
 bitmapAddr !i = (# idx, mask #)
@@ -110,6 +130,24 @@ instance PrimType Bool where
                 | setValue  = or# v mask
                 | otherwise = and# v (not# mask)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) =
+         bool# (0# /=# word2Int# (and# v mask))
+      where (# idx, mask #) = bitmapAddr n
+            !v = indexWord32OffAddr# addr idx
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 ->
+        case readWord32OffAddr# addr idx s1 of
+            (# s2, v #) -> (# s2, bool# (word2Int# (and# v mask) ==# 0#) #)
+      where (# !idx, !mask #) = bitmapAddr n
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) setValue = primitive $ \s1 ->
+        case readWord32OffAddr# addr idx s1 of
+            (# s2, v #) -> (# writeWord32OffAddr# addr idx (newVal v) s2, () #)
+      where (# !idx, !mask #) = bitmapAddr n
+            newVal v
+                | setValue  = or# v mask
+                | otherwise = and# v (not# mask)
+    {-# INLINE primAddrWrite #-}
 
 instance PrimType Word8 where
     sizeInBits _ = 8
@@ -120,6 +158,13 @@ instance PrimType Word8 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (W8# w) = primitive $ \s1 -> (# writeWord8Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = W8# (indexWord8OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readWord8OffAddr# addr n s1 in (# s2, W8# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (W8# w) = primitive $ \s1 -> (# writeWord8OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
+
 instance PrimType Word16 where
     sizeInBits _ = 16
     {-# INLINE sizeInBits #-}
@@ -129,6 +174,12 @@ instance PrimType Word16 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (W16# w) = primitive $ \s1 -> (# writeWord16Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = W16# (indexWord16OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readWord16OffAddr# addr n s1 in (# s2, W16# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (W16# w) = primitive $ \s1 -> (# writeWord16OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Word32 where
     sizeInBits _ = 32
     {-# INLINE sizeInBits #-}
@@ -138,6 +189,12 @@ instance PrimType Word32 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (W32# w) = primitive $ \s1 -> (# writeWord32Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = W32# (indexWord32OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readWord32OffAddr# addr n s1 in (# s2, W32# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (W32# w) = primitive $ \s1 -> (# writeWord32OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Word64 where
     sizeInBits _ = 64
     {-# INLINE sizeInBits #-}
@@ -147,6 +204,12 @@ instance PrimType Word64 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (W64# w) = primitive $ \s1 -> (# writeWord64Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = W64# (indexWord64OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readWord64OffAddr# addr n s1 in (# s2, W64# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (W64# w) = primitive $ \s1 -> (# writeWord64OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Int8 where
     sizeInBits _ = 8
     {-# INLINE sizeInBits #-}
@@ -156,6 +219,12 @@ instance PrimType Int8 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (I8# w) = primitive $ \s1 -> (# writeInt8Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = I8# (indexInt8OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readInt8OffAddr# addr n s1 in (# s2, I8# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (I8# w) = primitive $ \s1 -> (# writeInt8OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Int16 where
     sizeInBits _ = 16
     {-# INLINE sizeInBits #-}
@@ -165,6 +234,12 @@ instance PrimType Int16 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (I16# w) = primitive $ \s1 -> (# writeInt16Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = I16# (indexInt16OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readInt16OffAddr# addr n s1 in (# s2, I16# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (I16# w) = primitive $ \s1 -> (# writeInt16OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Int32 where
     sizeInBits _ = 32
     {-# INLINE sizeInBits #-}
@@ -174,6 +249,12 @@ instance PrimType Int32 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (I32# w) = primitive $ \s1 -> (# writeInt32Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = I32# (indexInt32OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readInt32OffAddr# addr n s1 in (# s2, I32# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (I32# w) = primitive $ \s1 -> (# writeInt32OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Int64 where
     sizeInBits _ = 64
     {-# INLINE sizeInBits #-}
@@ -183,6 +264,12 @@ instance PrimType Int64 where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (I64# w) = primitive $ \s1 -> (# writeInt64Array# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = I64# (indexInt64OffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readInt64OffAddr# addr n s1 in (# s2, I64# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (I64# w) = primitive $ \s1 -> (# writeInt64OffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 
 instance PrimType Float where
     sizeInBits _ = 32
@@ -193,6 +280,12 @@ instance PrimType Float where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (F# w) = primitive $ \s1 -> (# writeFloatArray# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = F# (indexFloatOffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readFloatOffAddr# addr n s1 in (# s2, F# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (F# w) = primitive $ \s1 -> (# writeFloatOffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
 instance PrimType Double where
     sizeInBits _ = 64
     {-# INLINE sizeInBits #-}
@@ -202,3 +295,9 @@ instance PrimType Double where
     {-# INLINE primMbaRead #-}
     primMbaWrite mba (I# n) (D# w) = primitive $ \s1 -> (# writeDoubleArray# mba n w s1, () #)
     {-# INLINE primMbaWrite #-}
+    primAddrIndex addr (I# n) = D# (indexDoubleOffAddr# addr n)
+    {-# INLINE primAddrIndex #-}
+    primAddrRead addr (I# n) = primitive $ \s1 -> let (# s2, r #) = readDoubleOffAddr# addr n s1 in (# s2, D# r #)
+    {-# INLINE primAddrRead #-}
+    primAddrWrite addr (I# n) (D# w) = primitive $ \s1 -> (# writeDoubleOffAddr# addr n w s1, () #)
+    {-# INLINE primAddrWrite #-}
