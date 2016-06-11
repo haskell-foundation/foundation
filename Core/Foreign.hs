a99e2d2adb70031cdf4269a145b2caedb16639e8
Vincent Hanquez <vincent@snarc.org>
2016-06-11 09:48:39 +0100

    export fileMapRead

--- Core/Foreign.hs
+++ Core/Foreign.hs
@@ -9,7 +9,9 @@ module Core.Foreign
     ( module Core.Primitive.FinalPtr
     , V.foreignMem
     , V.mutableForeignMem
+    , fileMapRead
     ) where
 
 import           Core.Primitive.FinalPtr
 import qualified Core.Vector.Unboxed as V
+import           Core.Foreign.MemoryMap
