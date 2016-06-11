c8844742c0315b9d87c50db9123d81d81673bfaa
Vincent Hanquez <vincent@snarc.org>
2016-06-11 06:56:04 +0100

    create a new Foreign to export all the foreign stuff

---
+++ Core/Foreign.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      : Core.Foreign
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : portable
+--
+module Core.Foreign
+    ( module Core.Primitive.FinalPtr
+    , V.foreignMem
+    , V.mutableForeignMem
+    ) where
+
+import           Core.Primitive.FinalPtr
+import qualified Core.Vector.Unboxed as V
