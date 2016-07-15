a878054aeb8b389fff11f34d0910e202730bda2e
Vincent Hanquez <vincent@snarc.org>
2016-07-15 06:07:19 +0100

    add a ForeignPtr handling to FinalPtr

--- Core/Primitive/FinalPtr.hs
+++ Core/Primitive/FinalPtr.hs
@@ -17,11 +17,13 @@ module Core.Primitive.FinalPtr
     , finalPtrSameMemory
     , castFinalPtr
     , toFinalPtr
+    , toFinalPtrForeign
     , withFinalPtr
     , withUnsafeFinalPtr
     ) where
 
 import GHC.Ptr
+import GHC.ForeignPtr
 import GHC.IO
 import Core.Primitive.Monad
 import Core.Internal.Primitive
@@ -29,6 +31,7 @@ import Core.Internal.Base (return, Bool(..), (==))
 
 -- | Create a pointer with an associated finalizer
 data FinalPtr a = FinalPtr (Ptr a)
+                | FinalForeign (ForeignPtr a)
 
 -- | Check if 2 final ptr points on the same memory bits
 --
@@ -36,7 +39,10 @@ data FinalPtr a = FinalPtr (Ptr a)
 -- and thus have the memory still valid, if 2 final ptrs have the
 -- same address, they should be the same final ptr
 finalPtrSameMemory :: FinalPtr a -> FinalPtr b -> Bool
-finalPtrSameMemory (FinalPtr p1) (FinalPtr p2) = p1 == (castPtr p2)
+finalPtrSameMemory (FinalPtr p1)     (FinalPtr p2)     = p1 == castPtr p2
+finalPtrSameMemory (FinalForeign p1) (FinalForeign p2) = p1 == castForeignPtr p2
+finalPtrSameMemory (FinalForeign _)  (FinalPtr _)      = False
+finalPtrSameMemory (FinalPtr _)      (FinalForeign _)  = False
 
 -- | create a new FinalPtr from a Pointer
 toFinalPtr :: PrimMonad prim => Ptr a -> (Ptr a -> IO ()) -> prim (FinalPtr a)
@@ -45,9 +51,14 @@ toFinalPtr ptr finalizer = unsafePrimFromIO (primitive makeWithFinalizer)
     makeWithFinalizer s =
         case compatMkWeak# ptr () (finalizer ptr) s of { (# s2, _ #) -> (# s2, FinalPtr ptr #) }
 
+-- | Create a new FinalPtr from a ForeignPtr
+toFinalPtrForeign :: ForeignPtr a -> FinalPtr a
+toFinalPtrForeign fptr = FinalForeign fptr
+
 -- | Cast a finalized pointer from type a to type b
 castFinalPtr :: FinalPtr a -> FinalPtr b
-castFinalPtr (FinalPtr a) = FinalPtr (castPtr a)
+castFinalPtr (FinalPtr a)     = FinalPtr (castPtr a)
+castFinalPtr (FinalForeign a) = FinalForeign (castForeignPtr a)
 
 -- | Looks at the raw pointer inside a FinalPtr, making sure the
 -- data pointed by the pointer is not finalized during the call to 'f'
@@ -55,6 +66,10 @@ withFinalPtr :: PrimMonad prim => FinalPtr p -> (Ptr p -> prim a) -> prim a
 withFinalPtr (FinalPtr ptr) f = do
     r <- f ptr
     primTouch ptr
+    return r
+withFinalPtr (FinalForeign fptr) f = do
+    r <- f (unsafeForeignPtrToPtr fptr)
+    unsafePrimFromIO (touchForeignPtr fptr)
     return r
 
 -- | Unsafe version of 'withFinalPtr'
