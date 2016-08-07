6b9ab14719d49ab6cca943b457d6f7bb3bb33146
Vincent Hanquez <vincent@snarc.org>
2016-08-07 17:32:34 +0100

    add explanation / warning about fileMapRead

--- Core/Foreign.hs
+++ Core/Foreign.hs
@@ -22,6 +22,18 @@ import qualified Core.Foreign.MemoryMap as I
 import qualified Prelude
 
 -- | Map in memory the whole content of a file
+--
+-- Note that the memory mapping is handled by the system, not at the haskell level.
+-- The system can modify the content of the memory as any moment under your feet.
+--
+-- It also have the limitation of your system, no emulation or nice handling of all
+-- those corners cases is attempted here.
+--
+-- for example mapping a large file (> 4G), on a 32 bits system is likely to just
+-- fail or returns inconsistent result.
+--
+-- In doubt, use 'readFile' or other simple routine that brings
+-- the content of the file in IO.
 fileMapRead :: FilePath -> IO (V.UArray Word8)
 fileMapRead fp = do
     (fptr, FileSize sz) <- I.fileMapRead fp
