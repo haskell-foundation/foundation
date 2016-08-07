f3972086244bdf7d5bf0efdd4ac8b726510d43fc
Vincent Hanquez <vincent@snarc.org>
2016-08-07 17:59:12 +0100

    fix warnings in Terminal

--- Core/IO/Terminal.hs
+++ Core/IO/Terminal.hs
@@ -10,20 +10,8 @@ module Core.IO.Terminal
     , putStr
     ) where
 
-import           System.IO (Handle, IOMode)
-import qualified System.IO as S
-import           Core.Collection
-import           Core.VFS
-import           Core.Internal.Types
 import           Core.Internal.Base
 import           Core.String
-import           Core.Array
-import           Core.Number
-import qualified Core.Array.Unboxed.Mutable as V
-import qualified Core.Array.Unboxed as V
-import qualified Core.String.UTF8 as S
-import           Control.Exception (bracket)
-import           Foreign.Ptr (plusPtr)
 import qualified Prelude
 
 -- | Print a string to standard output
