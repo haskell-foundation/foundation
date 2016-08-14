f7439ec5f3d8679edb16954f55471c7834b8530e
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-08-14 14:51:07 -0400

    Improve some docs in Foundation.VFS.Path

--- Foundation/VFS/Path.hs
+++ Foundation/VFS/Path.hs
@@ -21,6 +21,11 @@ module Foundation.VFS.Path
 
 import Foundation.Internal.Base
 
+-- $setup
+-- >>> import Foundation.Internal.Base
+-- >>> import Foundation.VFS.FilePath
+-- >>> import Foundation.VFS.Path
+
 -- | Path type class
 --
 -- defines the Path associated types and basic functions to implement related
@@ -28,12 +33,12 @@ import Foundation.Internal.Base
 --
 -- # TODO, add missing enhancement:
 --
--- ```
---    splitExtension :: PathEnt path -> (PathEnt path, PathEnt path)
---    addExtension  :: PathEnt path -> PathEnt path -> PathEnt path
---    (<.>) :: path -> PathEnt path -> path
---    (-<.>) :: path -> PathEnt path -> path
--- ```
+-- @
+-- splitExtension :: PathEnt path -> (PathEnt path, PathEnt path)
+-- addExtension  :: PathEnt path -> PathEnt path -> PathEnt path
+-- (<.>) :: path -> PathEnt path -> path
+-- (-<.>) :: path -> PathEnt path -> path
+-- @
 --
 class Path path where
     -- | the associated PathEntity of the given `path`
@@ -44,14 +49,14 @@ class Path path where
 
     -- | the associated prefix of the given `path`
     --
-    -- in the case of a `FilePath`, it is a void (i.e. `()`)
-    -- in the case of an `URI`, it is the schema, host, port...
+    -- in the case of a `Foundation.VFS.FilePath`, it is a void (i.e. `()`)
+    -- in the case of a `Foundation.VFS.URI`, it is the schema, host, port...
     type PathPrefix path
 
     -- | the associated suffix of the given path
     --
-    -- in the case of the `FilePath`, it is a void (i.e. `()`)
-    -- in the case of the `URI`, it is a the query, the fragment
+    -- in the case of the `Foundation.VFS.FilePath`, it is a void (i.e. `()`)
+    -- in the case of the `Foundation.VFS.URI`, it is a the query, the fragment
     type PathSuffix path
 
     -- | join a path entity to a given path
@@ -62,6 +67,7 @@ class Path path where
                          , [PathEnt path]
                          , PathSuffix path
                          )
+
     -- | build the path from the associated elements
     buildPath :: ( PathPrefix path
                  , [PathEnt path]
@@ -74,18 +80,30 @@ class Path path where
 --
 -- if you actually want to reference to the parent directory, simply uses:
 --
--- ```
+-- @
 -- parent "." /= "." </> ".."
--- ```
+-- @
+--
+-- >>> parent ("foo.hs" :: FilePath)
+-- .
+--
+-- >>> parent ("foo/bar/baz.hs" :: FilePath)
+-- foo/bar
 parent :: Path path => path -> path
 parent path = buildPath (p, init ps, s)
   where
-    (p, ps , s) = splitPath path
+    (p, ps, s) = splitPath path
 
 
 -- | get the filename of the given path
 --
--- If there is no filename, you will receive the mempty of the PathEnt
+-- If there is no filename, you will receive the 'mempty' of the 'PathEnt'
+--
+-- >>> filename ("foo.hs" :: FilePath)
+-- foo.hs
+--
+-- >>> filename ("foo/bar/baz.hs" :: FilePath)
+-- baz.hs
 filename :: (Path path, Monoid (PathEnt path)) => path -> PathEnt path
 filename path = case ps of
     [] -> mempty
@@ -108,16 +126,18 @@ last (_:xs) = last xs
 
 -- | get the path prefix information
 --
--- ```
--- prefix "/home/tab" == ()
--- ```
+-- >>> prefix ("/home/tab" :: FilePath)
+-- Absolute
+--
+-- >>> prefix ("home/tab" :: FilePath)
+-- Relative
 --
 -- or for URI (TODO, not yet accurate)
 --
--- ```
+-- @
 -- prefix "http://github.com/vincenthz/hs-foundation?w=1"
 --    == URISchema http Nothing Nothing "github.com" Nothing
--- ```
+-- @
 prefix :: Path path => path -> PathPrefix path
 prefix p = pre
   where
@@ -125,16 +145,15 @@ prefix p = pre
 
 -- | get the path suffix information
 --
--- ```
--- suffix "/home/tab" == ()
--- ```
+-- >>> suffix ("/home/tab" :: FilePath)
+-- ()
 --
 -- or for URI (TODO, not yet accurate)
 --
--- ```
+-- @
 -- suffix "http://github.com/vincenthz/hs-foundation?w=1"
 --    == URISuffix (["w", "1"], Nothing)
--- ```
+-- @
 suffix :: Path path => path -> PathSuffix path
 suffix p = suf
   where
