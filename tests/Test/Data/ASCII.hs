8776605296d4d4537a53a23a6f384962067b2cce
Nicolas DI PRIMA <nicolas@di-prima.fr>
2016-09-04 07:52:35 +0200

    use Foundation in tests

--- tests/Test/Data/ASCII.hs
+++ tests/Test/Data/ASCII.hs
@@ -2,10 +2,13 @@
 -- Module: Test.Data.ASCII
 --
 
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Test.Data.ASCII
     ( genAsciiChar
     ) where
 
+import Foundation
 import Test.Tasty.QuickCheck
 
 -- | a better generator for unicode Character
