79b5f9fcddba4fad6c4f851ac132892d23b6efef
Vincent Hanquez <vincent@snarc.org>
2016-09-23 20:07:56 +0100

    Add randomness test
    Basic entropy/compression/mean test, with fairly strict bounds.
    However it's possible that in some case, the test fails despite being
    perfectly random.
    
    The test is suppose to catch extremely bad entropy sources, for example
    programming or subsystem errors.

---
+++ tests/Test/Foundation/Random.hs
@@ -0,0 +1,90 @@
+    {-# LANGUAGE NoImplicitPrelude #-}
+module Test.Foundation.Random
+    ( testRandom
+    ) where
+
+import Imports
+import Foundation
+import Foundation.Primitive
+import Foundation.Array
+import Foundation.Collection
+import Foundation.System.Entropy
+import Control.Monad
+import qualified Prelude
+import qualified Data.List
+
+testRandom :: TestTree
+testRandom = testGroup "random"
+    [ testProperty "random" entropyCheck
+    ]
+  where
+    entropyCheck = monadicIO $ do
+        v <- run (getEntropy 1024 >>= randomTest)
+        --run (putStrLn . fromList $ show v)
+        assert (res_entropy v > 6.5 && res_entropy v <= 8)
+        assert (res_mean v >= 120 && res_mean v <= 136) 
+        assert (res_compressionPercent v >= 0 && res_compressionPercent v <= 5.0) 
+
+-------- generic random testing
+
+data RandomTestResult = RandomTestResult
+    { res_totalChars         :: Word64 -- ^ Total number of characters
+    , res_entropy            :: Double -- ^ Entropy per byte
+    , res_chi_square         :: Double -- ^ Chi Square
+    , res_mean               :: Double -- ^ Arithmetic Mean
+    , res_compressionPercent :: Double -- ^ Theorical Compression percent
+    , res_probs              :: [Double] -- ^ Probability of every bucket
+    } deriving (Show,Eq)
+
+-- | Mutable random test State
+newtype RandomTestState = RandomTestState (MUArray Word64 (PrimState IO))
+
+-- | Initialize new state to run tests
+randomTestInitialize :: IO RandomTestState
+randomTestInitialize = do
+    m <- mutNew 256
+    forM_ [0..255] $ \i -> mutWrite m i 0
+    return $ RandomTestState m
+
+-- | Append random data to the test state
+randomTestAppend :: RandomTestState -> UArray Word8 -> IO ()
+randomTestAppend (RandomTestState buckets) = mapM_ (addVec 1 . fromIntegral) . toList
+  where
+    addVec :: Word64 -> Int -> IO ()
+    addVec a i = mutRead buckets i >>= \d -> mutWrite buckets i $! d+a
+
+-- | Finalize random test state into some result
+randomTestFinalize :: RandomTestState -> IO RandomTestResult
+randomTestFinalize (RandomTestState buckets) = (calculate . toList) <$> freeze buckets
+
+randomTest :: UArray Word8 -> IO RandomTestResult
+randomTest a = do
+    st <- randomTestInitialize
+    randomTestAppend st a
+    randomTestFinalize st
+
+calculate :: [Word64] -> RandomTestResult
+calculate buckets = RandomTestResult
+    { res_totalChars = totalChars
+    , res_entropy    = entropy
+    , res_chi_square = chisq
+    , res_mean       = fromIntegral datasum Prelude./ fromIntegral totalChars
+    , res_compressionPercent = 100.0 * (8 - entropy) Prelude./ 8.0
+    , res_probs      = probs
+    }
+  where totalChars = Prelude.sum buckets
+        probs = fmap (\v -> fromIntegral v Prelude./ fromIntegral totalChars :: Double) buckets
+        entropy = Data.List.foldl' accEnt 0.0 probs
+        cexp    = fromIntegral totalChars Prelude./ 256.0 :: Double
+        (datasum, chisq) = Data.List.foldl' accMeanChi (0, 0.0) $ Prelude.zip [0..255] buckets
+        --chip' = abs (sqrt (2.0 * chisq) - sqrt (2.0 * 255.0 - 1.0))
+
+        accEnt ent pr
+            | pr > 0.0  = ent + (pr * xlog (1 Prelude./ pr))
+            | otherwise = ent
+        xlog v = Prelude.logBase 10 v * (Prelude.log 10 Prelude./ Prelude.log 2)
+
+        accMeanChi :: (Word64, Double) -> (Int, Word64) -> (Word64, Double)
+        accMeanChi (dataSum, chiSq) (i, ccount) =
+            let a      = fromIntegral ccount - cexp
+             in (dataSum + fromIntegral i * ccount, chiSq + (a * a Prelude./ cexp))
