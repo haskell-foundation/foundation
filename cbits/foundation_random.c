f982a47fa1562833cd184ce0f55b03c122dd96a7
Vincent Hanquez <vincent@snarc.org>
2016-09-25 10:15:24 +0100

    Fix support for linux getrandom syscall.
    sys/syscall.h need to be loaded before checking for SYS_getrandom
    being defined. Also fix some returns/errno value.

--- cbits/foundation_random.c
+++ cbits/foundation_random.c
@@ -2,34 +2,40 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include "foundation_system.h"
+#include <errno.h>
 
-#if defined(FOUNDATION_SYSTEM_LINUX) && defined(SYS_getrandom)
-#define _GNU_SOURCE
-
-#include <unistd.h>
+#if defined(FOUNDATION_SYSTEM_LINUX)
 #include <sys/syscall.h>
 #include <linux/random.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+
+#if defined(FOUNDATION_SYSTEM_LINUX) && defined(SYS_getrandom)
 int foundation_sysrandom_linux(void *buf, size_t length)
 {
 	unsigned int flags = 1; /* RANDOM=0x2, NONBLOCK=0x1 */
 	size_t i = 0;
 
-	if (length == 0)
-		return 0;
+	/* special case to detect availability */
+	if (length == 0) {
+		int r = syscall(SYS_getrandom, buf, 0, flags);
+		return (r == -1) ? -1 : 0;
+	}
 
 	while (i < length) {
 		int r = syscall(SYS_getrandom, buf + i, length - i, flags);
-		if (r <= 0 && r != -EAGAIN)
-			return r;
-		if (r != -EAGAIN)
+		if (r <= 0) {
+			if (errno != -EAGAIN)
+				return -errno;
+		}
+		if (r > 0)
 			i += r;
 	}
-	return r;
+	return i;
 }
 #else
-#include <errno.h>
-int foundation_sysrandom_linux(void *buf, size_t length)
-{
-	return -ENODEV;
-}
+int foundation_sysrandom_linux(void *buf, size_t length) { return -ENODEV; }
 #endif
