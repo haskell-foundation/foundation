c1bd2469e83a81471189380acef744c3d213651e
Vincent Hanquez <vincent@snarc.org>
2016-10-15 11:08:14 +0100

    [Random] Add bench for RNGv1

--- benchs/Sys.hs
+++ benchs/Sys.hs
@@ -7,6 +7,15 @@ import BenchUtil.Common
 import BenchUtil.RefData
 
 import Foundation.System.Entropy
+import Foundation.Random
+
+import qualified Prelude
+
+data NullRandom = NullRandom
+
+instance RandomGen NullRandom where
+    randomNew        = return NullRandom
+    randomGenerate n r = (fromList (Prelude.replicate n 0), r)
 
 benchSys =
     [ bgroup "Random"
@@ -14,4 +23,12 @@ benchSys =
         , bench "Entropy-16"   $ whnfIO $ getEntropy 16
         , bench "Entropy-1024" $ whnfIO $ getEntropy 1024
         ]
+    , bgroup "RNGv1"
+        [ bench "Entropy-1"    $ benchRandom 1 randomNew (Proxy :: Proxy RNGv1)
+        , bench "Entropy-1024"    $ benchRandom 1024 randomNew (Proxy :: Proxy RNGv1)
+        , bench "Entropy-1M"    $ benchRandom (1024 * 1024) randomNew (Proxy :: Proxy RNGv1)
+        ]
     ]
+
+benchRandom :: RandomGen rng => Int -> MonadRandomState NullRandom rng -> Proxy rng -> Benchmarkable
+benchRandom n rNew _ = whnf (fst . randomGenerate n) (fst $ withRandomGenerator NullRandom rNew)
