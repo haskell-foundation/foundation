b708fa64159ef89f957b3307ff3200d5bd075c0f
Vincent Hanquez <vincent@snarc.org>
2016-10-29 06:52:35 +0100

    add an implementation for Natural

--- Foundation/Internal/Natural.hs
+++ Foundation/Internal/Natural.hs
@@ -10,7 +10,27 @@ import Numeric.Natural
 
 #else
 
+import Prelude (Show,Eq,Ord,Enum,Num(..),Integer,error,(<), (>), otherwise)
+import Data.Typeable
+
 newtype Natural = Natural Integer
-    deriving (Show,Eq,Ord)
+    deriving (Show,Eq,Ord,Enum,Typeable)
+
+-- re-create the buggy Num instance for Natural
+instance Num Natural where
+    fromInteger n
+        | n < 0     = error "natural should be positive: "
+        | otherwise = Natural n
+    (+) (Natural a) (Natural b) = Natural (a + b)
+    (-) (Natural a) (Natural b)
+        | r < 0     = error "natural should be positve"
+        | otherwise = Natural (a - b)
+      where r = (a - b)
+    (*) (Natural a) (Natural b) = Natural (a * b)
+    abs n = n
+    negate n = n
+    signum (Natural n)
+        | n > 0     = 1
+        | otherwise = 0
 
 #endif
