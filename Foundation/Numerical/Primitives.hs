88fab2679a0923c45b19c723a7ddb816fec9440e
Vincent Hanquez <vincent@snarc.org>
2016-11-07 11:04:44 +0000

    add Integral, Int{8,16,32,64} and Natural to Hashable

--- Foundation/Numerical/Primitives.hs
+++ Foundation/Numerical/Primitives.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE MagicHash #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Foundation.Numerical.Primitives
     ( intToWord
+    , integralConvert
     ) where
 
 import GHC.Types
 import GHC.Prim
---import GHC.Word
---import GHC.Int
+import GHC.Word
+import GHC.Int
 
 intToWord :: Int -> Word
 intToWord (I# i) = W# (int2Word# i)
 {-# INLINE intToWord #-}
+
+class IntegralConvert a b where
+    -- lossless integral convertion
+    integralConvert :: a -> b
+
+instance IntegralConvert Int8 Word8 where
+    integralConvert (I8# i) = W8# (int2Word# i)
+instance IntegralConvert Int16 Word16 where
+    integralConvert (I16# i) = W16# (int2Word# i)
+instance IntegralConvert Int32 Word32 where
+    integralConvert (I32# i) = W32# (int2Word# i)
+instance IntegralConvert Int64 Word64 where
+    integralConvert (I64# i) = W64# (int2Word# i)
