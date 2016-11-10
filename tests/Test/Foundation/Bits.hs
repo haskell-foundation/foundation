5626bfd28d8cf1d55d03f313f71e168de2c57f7d
Vincent Hanquez <vincent@snarc.org>
2016-11-10 07:15:41 +0000

    fix test

--- tests/Test/Foundation/Bits.hs
+++ tests/Test/Foundation/Bits.hs
@@ -6,10 +6,11 @@ module Test.Foundation.Bits
 import Foundation.Bits
 import Imports
 import Foundation
+import Foundation.Numerical
 
 tests = testGroup "Bits"
-    [ testProperty "round-up" $ \(Positive m) n' -> (n' >= 1 && n' < 8) ==>
-        let n = 2 ^ (n' :: Word)
+    [ testProperty "round-up" $ \(Positive m) n' -> n' >= 1 ==>
+        let n = 2 ^ ((n' `mod` 30) :: Word)
             md = alignRoundUp m n
          in (md `mod` n) == 0 && md >= m
     ]
