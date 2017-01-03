8446aa2edbff2fc8e58f76ef3a6babb06025d816
Vincent Hanquez <vincent@snarc.org>
2017-01-03 09:06:10 +0000

    cleanup & add documentation

--- Foundation/Monad/Exception.hs
+++ Foundation/Monad/Exception.hs
@@ -8,23 +8,33 @@ module Foundation.Monad.Exception
 import           Foundation.Internal.Base
 import qualified Control.Exception (throwIO, catch)
 
+-- | Monad that can represent failure
+--
+-- Similar to MonadFail but with a parametrized Failure linked to the Monad
 class Monad m => MonadFailure m where
+    -- | The associated type with the MonadFailure, representing what
+    -- failure can be encoded in this monad
     type Failure m
-    failure :: Failure m -> m ()
 
+    -- | Raise a Failure through a monad.
+    mFail :: Failure m -> m ()
+
+-- | Monad that can throw exception
 class Monad m => MonadThrow m where
-    throw :: Exception e => e -> m ()
+    -- | Throw immediatity an exception.
+    -- Only a 'MonadCatch' monad will be able to catch the exception using 'catch'
+    throw :: Exception e => e -> m a
 
--- | Monad that can catch
+-- | Monad that can catch exception
 class MonadThrow m => MonadCatch m where
     catch :: Exception e => m a -> (e -> m a) -> m a
 
 instance MonadFailure Maybe where
     type Failure Maybe = ()
-    failure _ = Nothing
+    mFail _ = Nothing
 instance MonadFailure (Either a) where
     type Failure (Either a) = a
-    failure a = Left a
+    mFail a = Left a
 
 instance MonadThrow IO where
     throw = Control.Exception.throwIO
