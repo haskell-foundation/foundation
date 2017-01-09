be6dd4e42c40595036bc9d03cdfda2d836ac1f3f
Vincent Hanquez <vincent@snarc.org>
2017-01-09 15:58:54 +0000

    add compatibility for old error

--- Foundation/Internal/Error.hs
+++ Foundation/Internal/Error.hs
@@ -2,16 +2,29 @@
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ImplicitParams #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE CPP #-}
 module Foundation.Internal.Error
     ( error
     ) where
 
-import           GHC.Exception( errorCallWithCallStackException )
 import           GHC.Prim
 import           GHC.Types (RuntimeRep)
 import           Foundation.String.UTF8
 import           Foundation.Internal.CallStack
 
+#if MIN_VERSION_base(4,9,0)
+
+import           GHC.Exception (errorCallWithCallStackException)
+
 -- | stop execution and displays an error message
 error :: forall (r :: RuntimeRep) . forall (a :: TYPE r) . HasCallStack => String -> a
 error s = raise# (errorCallWithCallStackException (sToList s) ?callstack)
+
+#else
+
+import           GHC.Exception (errorCallException)
+
+error :: String -> a
+error s = raise# (errorCallException (sToList s))
+
+#endif
