158700910914edb44bd7230061bba8147c6f1c4c
Nicolas DI PRIMA <nicolas@di-prima.fr>
2017-01-18 21:21:45 +0000

    use the correct tools from quickcheck

--- tests/Test/Foundation/Network/IPv4.hs
+++ tests/Test/Foundation/Network/IPv4.hs
@@ -16,23 +16,23 @@ import Test.Foundation.Storable
 -- | test property equality for the given Collection
 testEquality :: Gen IPv4 -> TestTree
 testEquality genElement = testGroup "equality"
-    [ testProperty "x == x" $ forAll genElement (\x -> x == x)
+    [ testProperty "x == x" $ forAll genElement (\x -> x === x)
     , testProperty "x == y" $ forAll ((,) <$> genElement <*> genElement) $
-        \(x,y) -> (toTuple x == toTuple y) == (x == y)
+        \(x,y) -> (toTuple x == toTuple y) === (x == y)
     ]
 
 -- | test ordering
 testOrdering :: Gen IPv4 -> TestTree
 testOrdering genElement = testProperty "ordering" $
     forAll ((,) <$> genElement <*> genElement) $ \(x, y) ->
-        (toTuple x `compare` toTuple y) == x `compare` y
+        (toTuple x `compare` toTuple y) === x `compare` y
 
 testNetworkIPv4 :: TestTree
 testNetworkIPv4 = testGroup "IPv4"
     [ testProperty "toTuple . fromTuple == id" $
-        forAll genIPv4Tuple $ \x -> x == toTuple (fromTuple x)
+        forAll genIPv4Tuple $ \x -> x === toTuple (fromTuple x)
     , testProperty "toString . fromString == id" $
-        forAll genIPv4String $ \x -> x == toString (fromString $ toList x)
+        forAll genIPv4String $ \x -> x === toString (fromString $ toList x)
     , testEquality genIPv4
     , testOrdering genIPv4
     , testPropertyStorable      "Storable" (Proxy :: Proxy IPv4)
