2cf8297c242b8b14d3484c791156a73b172ca859
Vincent Hanquez <vincent@snarc.org>
2017-02-25 10:49:15 +0000

    add system bindings

---
+++ Foundation/System/Bindings.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE CPP #-}
+module Foundation.System.Bindings
+    ( module X
+    ) where
+
+#ifdef mingw32_HOST_OS
+import Foundation.System.Bindings.Windows as X
+#else
+import Foundation.System.Bindings.Posix as X
+#endif
