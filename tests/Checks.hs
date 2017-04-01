67ea61346ca10a5433a3ae84f8338645cfcf235d
Vincent Hanquez <vincent@snarc.org>
2017-04-01 21:15:12 +0100

    fix test/add test

--- tests/Checks.hs
+++ tests/Checks.hs
@@ -6,12 +6,22 @@ import Foundation.Check
 
 testAdditive :: forall a . (Eq a, Additive a, Arbitrary a) => Proxy a -> Test
 testAdditive _ = Group "Additive"
-    [ Unit "eq"                 $ azero === azero
+    [ Property "eq"             $ azero === (azero :: a)
     , Property "a + azero == a" $ \(v :: a)     -> v + azero === v
     , Property "azero + a == a" $ \(v :: a)     -> azero + v === v
     , Property "a + b == b + a" $ \(v1 :: a) v2 -> v1 + v2 === v2 + v1
     ]
 
 main = defaultMain $ Group "foundation"
-    [ testAdditive (Proxy :: Proxy Int)
+    [ Group "Numerical"
+        [ Group "Int"
+            [ testAdditive (Proxy :: Proxy Int)
+            ]
+        , Group "Word64"
+            [ testAdditive (Proxy :: Proxy Word64)
+            ]
+        , Group "Natural"
+            [ testAdditive (Proxy :: Proxy Natural)
+            ]
+        ]
     ]
