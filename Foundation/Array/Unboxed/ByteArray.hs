c23be016abbd2b2a9eff6756f6b095eb346a34f8
Vincent Hanquez <vincent@snarc.org>
2017-04-05 23:28:30 +0100

    hide unused functions

--- Foundation/Array/Unboxed/ByteArray.hs
+++ Foundation/Array/Unboxed/ByteArray.hs
@@ -1,15 +1,14 @@
 module Foundation.Array.Unboxed.ByteArray
     ( MutableByteArray
     , mutableByteArraySet
-    , mutableByteArraySetBetween
-    , mutableByteArrayMove
+    -- , mutableByteArraySetBetween
+    -- , mutableByteArrayMove
     ) where
 
 import Foundation.Internal.Base
+import Foundation.Internal.Types
 import Foundation.Primitive.Monad
-import Foundation.Array.Common
 import Foundation.Array.Unboxed.Mutable
-import Foundation.Numerical
 import Control.Monad (forM_)
 
 -- | Mutable Byte Array alias
@@ -18,19 +17,21 @@ type MutableByteArray st = MUArray Word8 st
 mutableByteArraySet :: PrimMonad prim => MUArray Word8 (PrimState prim) -> Word8 -> prim ()
 mutableByteArraySet mba val = do
     -- naive haskell way. TODO: call memset or a 32-bit/64-bit method
-    forM_ [0..(len-1)] $ \i -> unsafeWrite mba i val
+    forM_ [0..(sizeLastOffset len)] $ \i -> unsafeWrite mba i val
   where
-    len = mutableLength mba
+    len = mutableLengthSize mba
 
-mutableByteArraySetBetween :: PrimMonad prim => MUArray Word8 (PrimState prim) -> Word8 -> Int -> Int -> prim ()
+{-
+mutableByteArraySetBetween :: PrimMonad prim => MUArray Word8 (PrimState prim) -> Word8 -> Offset Word8 -> Size Word8 -> prim ()
 mutableByteArraySetBetween mba val offset size
-    | offset < 0                        = throw (OutOfBound OOB_MemSet offset len)
-    | offset > len || offset+size > len = throw (OutOfBound OOB_MemSet (offset+size) len)
+    | offset < 0                        = primOutOfBound OOB_MemSet offset                      len
+    | offset > len || offset+size > len = primOutOfBound OOB_MemSet (offset `OffsetPlusE` size) len
     | otherwise =
         -- TODO same as mutableByteArraySet
-        forM_ [offset..(offset+size-1)] $ \i -> unsafeWrite mba i val
+        forM_ [offset..(offset + sizeLastOffset size)] $ \i -> unsafeWrite mba i val
   where
-    len = mutableLength mba
+    len = mutableLengthSize mba
 
 mutableByteArrayMove :: PrimMonad prim => MUArray Word8 (PrimState prim) -> Int -> Int -> Int -> prim ()
 mutableByteArrayMove _mba _ofs _sz = undefined
+    -}
