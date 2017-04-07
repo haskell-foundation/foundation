67e2b6d69b3741bdcde02f0916a07f18b5776afd
Vincent Hanquez <vincent@snarc.org>
2017-04-07 17:55:50 +0100

    Improve property by keeping the arguments generated as Strings and the type of test

--- Foundation/Check/Property.hs
+++ Foundation/Check/Property.hs
@@ -1,5 +1,8 @@
 module Foundation.Check.Property
     ( Property(..)
+    , PropertyTestArg(..)
+    , PropertyTestCheck(..)
+    , PropertyTestResult(..)
     , IsProperty
     , property
     -- * Properties
@@ -10,24 +13,46 @@ module Foundation.Check.Property
 import Foundation.Internal.Base
 import Foundation.Check.Gen
 import Foundation.Check.Arbitrary
+import Foundation.String
+
+-- | The type of check this test did for a property
+data PropertyTestCheck = PropertyBoolean
+                       | PropertyEquality String String
+
+-- | A linked-list of arguments to this test
+data PropertyTestArg = PropertyEOA PropertyTestCheck
+                     | PropertyArg String PropertyTestArg
+
+-- | Whether this property test failed or succeed
+data PropertyTestResult = PropertyTestFailed | PropertyTestSuccess
+    deriving (Show,Eq)
+
+data Property = Prop { unProp :: Gen (PropertyTestResult, PropertyTestArg) }
 
 class IsProperty p where
     property :: p -> Property
 
+boolToTestResult :: Bool -> PropertyTestResult
+boolToTestResult True  = PropertyTestSuccess
+boolToTestResult False = PropertyTestFailed
+
 instance IsProperty Bool where
-    property b = Prop (pure b)
+    property b = Prop (pure (boolToTestResult b, PropertyEOA PropertyBoolean))
 instance IsProperty Property where
-    property p = p -- (Prop result) = Prop . return $ result
-instance IsProperty prop => IsProperty (Gen prop) where
-    property mp = Prop (mp >>= \p -> unProp (property p))
-instance (Arbitrary a, IsProperty prop) => IsProperty (a -> prop) where
+    property p = p
+instance (Show a, Arbitrary a, IsProperty prop) => IsProperty (a -> prop) where
     property p = forAll arbitrary p
 
-data Property = Prop { unProp :: Gen Bool }
+forAll :: (Show a, IsProperty prop) => Gen a -> (a -> prop) -> Property
+forAll generator tst = Prop $ do
+    a <- generator
+    augment a <$> unProp (property (tst a))
+  where
+    augment a (tr, arg) = (tr, PropertyArg (fromList $ show a) arg)
 
-forAll :: IsProperty prop => Gen a -> (a -> prop) -> Property
-forAll generator tst = Prop (generator >>= \a -> unProp (property (tst a)))
-
-(===) :: Eq a => a -> a -> Property
-(===) a b = Prop (pure (a == b))
+(===) :: (Show a, Eq a) => a -> a -> Property
+(===) a b = -- Prop (pure (a == b, show a `==` show b )
+    let sa = fromList (show a)
+        sb = fromList (show b)
+     in Prop $ pure (boolToTestResult (a == b), PropertyEOA $ PropertyEquality sa sb)
 infix 4 ===
