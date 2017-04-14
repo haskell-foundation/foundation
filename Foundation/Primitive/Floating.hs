156303bf1a52f3d4f88911cc6ffd6a70308f7000
Vincent Hanquez <vincent@snarc.org>
2017-04-14 22:21:09 +0100

    Add some Floating primitive

---
+++ Foundation/Primitive/Floating.hs
@@ -0,0 +1,21 @@
+module Foundation.Primitive.Floating
+    ( integerToDouble
+    , naturalToDouble
+    , doubleExponant
+    ) where
+
+import           GHC.Types
+import           Foundation.Internal.Base
+import           Foundation.Internal.Natural
+import qualified Prelude (fromInteger, toInteger, (^^))
+
+integerToDouble :: Integer -> Double
+integerToDouble = Prelude.fromInteger
+-- this depends on integer-gmp
+--integerToDouble i = D# (doubleFromInteger i)
+
+naturalToDouble :: Natural -> Double
+naturalToDouble = integerToDouble . Prelude.toInteger
+
+doubleExponant :: Double -> Int -> Double
+doubleExponant = (Prelude.^^)
