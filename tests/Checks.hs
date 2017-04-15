e0efb589b759e24ac6e077b1c7be2be5a3b8561d
Vincent Hanquez <vincent@snarc.org>
2017-04-15 21:48:04 +0100

    Update tests

--- tests/Checks.hs
+++ tests/Checks.hs
@@ -5,6 +5,7 @@ import Foundation
 import Foundation.Primitive
 import Foundation.Check
 import Foundation.String.Read
+import Foundation.Numerical
 import qualified Prelude
 
 testAdditive :: forall a . (Show a, Eq a, Additive a, Arbitrary a) => Proxy a -> Test
@@ -15,17 +16,17 @@ testAdditive _ = Group "Additive"
     , Property "a + b == b + a" $ \(v1 :: a) v2 -> v1 + v2 === v2 + v1
     ]
 
-readFloatingExact' :: String -> Maybe (Integer, Maybe (Word, Natural), Maybe Int)
-readFloatingExact' s = readFloatingExact s (\x y z -> Just (x,y,z))
+readFloatingExact' :: String -> Maybe (Bool, Natural, Maybe (Word, Natural), Maybe Int)
+readFloatingExact' s = readFloatingExact s (\s x y z -> Just (s,x,y,z))
 
 doubleEqualApprox :: Double -> Double -> PropertyCheck
-doubleEqualApprox d1 d2 = (propertyCompare pName1 (<) (negate lim) d) `propertyAnd` (propertyCompare pName2 (>) lim d)
-  where d = d2 - d1
-        pName1 = show d1 <> " - " <> show d2 <> " < " <> show (negate lim)
-        pName2 = show d1 <> " - " <> show d2 <> " > " <> show lim
+doubleEqualApprox d1 d2 = (propertyCompare pName1 (<) (negate lim) d) `propertyAnd` (propertyCompare pName2 (<) d lim)
+  where 
+        d = d2 - d1
+
+        pName1 = show (negate lim) <> " < " <> show d2 <> " - " <> show d1 <> " (== " <> show d <> " )"
+        pName2 = show d1 <> " - " <> show d2 <> " (== " <> show d <> " )" <> " < " <> show lim
         lim = 1.0e-8
-
-
 
 main = defaultMain $ Group "foundation"
     [ Group "Numerical"
@@ -49,18 +50,20 @@ main = defaultMain $ Group "foundation"
                 , Property "just-sign"     $ readFloatingExact' "-" === Nothing
                 , Property "extra-content" $ readFloatingExact' "-123a" === Nothing
                 , Property "no-dot-after"  $ readFloatingExact' "-123." === Nothing
-                , Property "case1"         $ readFloatingExact' "-123.1" === Just (-123, Just (1, 1), Nothing)
-                , Property "case2"         $ readFloatingExact' "10001.001" === Just (10001, Just (3, 1), Nothing)
-                , Property "any"           $ \i (v :: Word8) n ->
+                , Property "case1"         $ readFloatingExact' "-123.1" === Just (True, 123, Just (1, 1), Nothing)
+                , Property "case2"         $ readFloatingExact' "10001.001" === Just (False, 10001, Just (3, 1), Nothing)
+                , Property "any"           $ \s i (v :: Word8) n ->
                                                 let vw = integralUpsize v :: Word
                                                     sfloat = show n
                                                     digits = integralCast (length sfloat) + vw
-                                                 in readFloatingExact' (show i <> "." <> replicate vw '0' <> sfloat) === Just (i, Just (digits, n), Nothing)
+                                                 in readFloatingExact' ((if s then "-" else "") <> show i <> "." <> replicate vw '0' <> sfloat) === Just (s, i, Just (digits, n), Nothing)
                 ]
             , Group "Double"
                 [ Property "case1" $ readDouble "96152.5" === Just 96152.5
-                , Property "case2" $ maybe (propertyFail "Nothing") (doubleEqualApprox 123.0e100) $ readDouble "123e100"
-                , Property "case3" $ maybe (propertyFail "Nothing") (doubleEqualApprox 5.719543847727751e-37) $ readDouble "5.719543847727751E-37"
+                , Property "case2" $ maybe (propertyFail "Nothing") (doubleEqualApprox 1.2300000000000002e102) $ readDouble "123e100"
+                , Property "case3" $ maybe (propertyFail "Nothing") (doubleEqualApprox 0.00001204) $ readDouble "0.00001204"
+                , Property "case4" $ maybe (propertyFail "Nothing") (doubleEqualApprox 2.5e12) $ readDouble "2.5e12"
+                , Property "case5" $ maybe (propertyFail "Nothing") (doubleEqualApprox 6.0e-4) $ readDouble "6.0e-4"
                 , Property "Prelude.read" $ \(d :: Double) -> case readDouble (show d) of
                                                                   Nothing -> propertyFail "Nothing"
                                                                   Just d' -> d' `doubleEqualApprox` (Prelude.read $ toList $ show d)
