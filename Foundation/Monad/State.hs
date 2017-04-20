c5be333bdddfc0c28d555f3d1c5c0920aa1d57a8
Nicolas DI PRIMA <nicolas@primetype.co.uk>
2017-04-20 21:04:57 +0100

    add MonadState
    fix #246

--- Foundation/Monad/State.hs
+++ Foundation/Monad/State.hs
@@ -1,11 +1,65 @@
 {-# LANGUAGE TupleSections #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 module Foundation.Monad.State
-    ( StateT
+    ( -- * MonadState
+      MonadState(..)
+    , modify
+    , modify'
+    , gets
+
+    , -- * StateT
+      StateT
     , runStateT
     ) where
 
-import Foundation.Internal.Base (($), (.))
+import Foundation.Internal.Base (($), (.), seq)
 import Foundation.Monad.Base
+
+class Monad m => MonadState s m | m -> s where
+    {-# MINIMAL state | get, put #-}
+    -- | Return the state from the internals of the monad.
+    get :: m s
+    get = state (\s -> (s, s))
+
+    -- | Replace the state inside the monad.
+    put :: s -> m ()
+    put s = state (\_ -> ((), s))
+
+    -- | Embed a simple state action into the monad.
+    state :: (s -> (a, s)) -> m a
+    state f = do
+        s <- get
+        let ~(a, s') = f s
+        put s'
+        return a
+
+-- | Monadic state transformer.
+--
+--      Maps an old state to a new state inside a state monad.
+--      The old state is thrown away.
+--
+-- >      Main> :t modify ((+1) :: Int -> Int)
+-- >      modify (...) :: (MonadState Int a) => a ()
+--
+--    This says that @modify (+1)@ acts over any
+--    Monad that is a member of the @MonadState@ class,
+--    with an @Int@ state.
+modify :: MonadState s m => (s -> s) -> m ()
+modify f = state (\s -> ((), f s))
+
+-- | A variant of 'modify' in which the computation is strict in the
+-- new state.
+modify' :: MonadState s m => (s -> s) -> m ()
+modify' f = state (\s -> let s' = f s in s' `seq` ((), s'))
+
+-- | Gets specific component of the state, using a projection function
+-- supplied.
+gets :: MonadState s m => (s -> a) -> m a
+gets f = do
+    s <- get
+    return (f s)
 
 -- | State Transformer
 newtype StateT s m a = StateT { runStateT :: s -> m (a, s) }
@@ -46,3 +100,8 @@ instance (Functor m, MonadThrow m) => MonadThrow (StateT s m) where
 
 instance (Functor m, MonadCatch m) => MonadCatch (StateT s m) where
     catch (StateT m) c = StateT $ \s1 -> m s1 `catch` (\e -> runStateT (c e) s1)
+
+instance Monad m => MonadState s (StateT s m) where
+  get = state $ \ s -> (s, s)
+  state f = StateT (return . f)
+  put s = state $ \ _ -> ((), s)
