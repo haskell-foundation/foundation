af3c054982f6b2da3412ad1aaa4bb94b3a34a8e9
Nicolas DI PRIMA <nicolas@primetype.co.uk>
2017-04-25 21:26:47 +0100

    update fundation tests

--- Foundation/Check.hs
+++ Foundation/Check.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Foundation.Check
     ( Gen
     , Arbitrary(..)
@@ -36,6 +37,8 @@ import           Foundation.Check.Arbitrary
 import           Foundation.Check.Property
 import           Foundation.Random
 import           Foundation.Monad
+import           Foundation.Monad.State
+import           Foundation.List.DList
 import           Control.Exception (evaluate, SomeException)
 import           System.Exit
 
@@ -54,18 +57,6 @@ testName (Unit s _)     = s
 testName (Property s _) = s
 testName (Group s _)    = s
 
-data Context = Context
-    { contextLevel  :: !Word
-    , contextGroups :: [String]
-    , contextSeed   :: !Word64
-    }
-
-appendContext :: String -> Context -> Context
-appendContext s ctx = ctx
-    { contextLevel = 1 + contextLevel ctx
-    , contextGroups = s : contextGroups ctx
-    }
-
 data PropertyResult =
       PropertySuccess
     | PropertyFailed  String
@@ -83,24 +74,162 @@ nbFail (PropertyResult _ _ (PropertyFailed _)) = 1
 nbFail (PropertyResult _ _ PropertySuccess)    = 0
 nbFail (GroupResult    _ t _)                  = t
 
--- | return the number of tests runned and the result
-runProp :: Context -> String -> Property -> IO (PropertyResult, Word64)
-runProp ctx s prop = iterProp 1
+-- | Run tests
+defaultMain :: Test -> IO ()
+defaultMain t = do
+    -- parse arguments
+    --let arguments = [ "seed", "j" ]
+
+    -- generate a new seed
+    seed <- getRandomPrimType
+
+    (_, cfg) <- runStateT (runCheck $ test t) $ defaultConfig seed
+    let oks = testPassed cfg
+        kos = testFailed cfg
+        tot = oks + kos
+    if kos > 0
+        then do
+          putStrLn $ "Failed " <> fromList (show kos) <> " out of " <> fromList (show tot)
+          exitFailure
+        else do
+          putStrLn $ "Succeed " <> fromList (show oks) <> " test(s)"
+          exitSuccess
+
+-- | internal check monad for facilitating the tests traversal
+newtype Check a = Check { runCheck :: StateT Config IO a }
+  deriving (Functor, Applicative, Monad, MonadIO)
+instance MonadState Check where
+    type State Check = Config
+    withState = Check . withState
+
+type Seed = Word64
+data Config = Config
+    { testPath     :: !(DList String)
+        -- ^ for internal use when pretty printing
+    , indent       :: !Word
+        -- ^ for internal use when pretty printing
+    , testPassed   :: !Word
+    , testFailed   :: !Word
+    , getSeed      :: !Seed
+        -- ^ the seed for the tests
+    , getGenParams :: !GenParams
+        -- ^ Parameters for the generator
+        --
+        -- default:
+        --   * 32bits long numbers;
+        --   * array of 512 elements max;
+        --   * string of 8192 bytes max.
+        --
+    , numTests     :: !Word64
+        -- ^ the number of tests to perform on every property.
+        --
+        -- default: 100
+    , displayOptions :: ![DisplayOption]
+    }
+
+data DisplayOption
+    = DisplayTerminalErrorOnly
+    | DisplayTerminalVerbose
+  deriving (Eq, Show)
+
+onDisplayOption :: DisplayOption -> Check () -> Check ()
+onDisplayOption opt chk = do
+    on <- elem opt . displayOptions <$> get
+    if on then chk else return ()
+
+whenErrorOnly :: Check () -> Check ()
+whenErrorOnly = onDisplayOption DisplayTerminalErrorOnly
+
+whenVerbose :: Check () -> Check ()
+whenVerbose = onDisplayOption DisplayTerminalVerbose
+
+passed :: Check ()
+passed = withState $ \s -> ((), s { testPassed = testPassed s + 1 })
+
+failed :: Check ()
+failed = withState $ \s -> ((), s { testFailed = testFailed s + 1 })
+
+-- | create the default configuration
+--
+-- see @Config@ for details
+defaultConfig :: Seed -> Config
+defaultConfig s = Config
+    { testPath     = mempty
+    , indent       = 0
+    , testPassed   = 0
+    , testFailed   = 0
+    , getSeed      = s
+    , getGenParams = params
+    , numTests     = 100
+    , displayOptions = [DisplayTerminalErrorOnly]
+    }
   where
-    nbTests = 100
-    iterProp :: Word64 -> IO (PropertyResult, Word64)
-    iterProp i
-        | i == nbTests = return (PropertySuccess, i)
-        | otherwise    = do
-            r <- toResult i
-            case r of
-                (PropertyFailed e, _)               -> return (PropertyFailed e, i)
-                (PropertySuccess, cont) | cont      -> iterProp (i+1)
-                                        | otherwise -> return (PropertySuccess, i)
-    toResult :: Word64 -> IO (PropertyResult, Bool)
-    toResult it =
-                (propertyToResult <$> evaluate (runGen (unProp prop) (rngIt it) params))
-        `catch` (\(e :: SomeException) -> return (PropertyFailed (fromList $ show e), False))
+    params = GenParams
+        { genMaxSizeIntegral = 32   -- 256 bits maximum numbers
+        , genMaxSizeArray    = 512  -- 512 elements
+        , genMaxSizeString   = 8192 -- 8K string
+        }
+
+test :: Test -> Check TestResult
+test (Group s l) = pushGroup s l
+test (Unit _ _) = undefined
+test (Property name prop) = do
+    whenVerbose $ displayCurrentProperty name
+    r'@(PropertyResult _ _ r) <- testProperty name (property prop)
+    whenErrorOnly $ case r of
+        PropertySuccess  -> return ()
+        PropertyFailed w -> displayPropertyFailed name w
+    return r'
+
+displayCurrentProperty :: String -> Check ()
+displayCurrentProperty name = do
+    path <- toList . testPath <$> get
+    liftIO $ putStrLn $ intercalate "::" $ path <> [name]
+
+displayPropertyFailed :: String -> String -> Check ()
+displayPropertyFailed name w = do
+    path <- toList . testPath <$> get
+    liftIO $ do
+        putStrLn $ (intercalate "::" $ path <> [name]) <> " ERROR"
+        putStrLn w
+
+pushGroup :: String -> [Test] -> Check TestResult
+pushGroup name list = do
+    withState $ \s -> ((), s { testPath = push (testPath s) name, indent = indent s + 1 })
+    results <- mapM test list
+    withState $ \s -> ((), s { testPath = pop (testPath s), indent = indent s - 1 })
+    return $ GroupResult name (foldl' (+) 0 $ fmap nbFail results) results
+  where
+    push = snoc
+    pop c = case unsnoc c of
+        Nothing     -> mempty
+        Just (c, _) -> c
+
+testProperty :: String -> Property -> Check TestResult
+testProperty name prop = do
+    seed <- getSeed <$> get
+    path <- testPath <$> get
+    let rngIt = genRng seed (name : toList path)
+
+    maxTests <- numTests <$> get
+
+    (res, nbTests) <- iterProp 1 maxTests rngIt
+    return (PropertyResult name nbTests res)
+  where
+    iterProp !n !limit !rngIt
+      | n == limit = passed >> return (PropertySuccess, n)
+      | otherwise  = do
+          params <- getGenParams <$> get
+          r <- liftIO $ toResult n params
+          case r of
+              (PropertyFailed e, _)               -> failed >> return (PropertyFailed e, n)
+              (PropertySuccess, cont) | cont      -> iterProp (n+1) limit rngIt
+                                      | otherwise -> passed >> return (PropertySuccess, n)
+        where
+          toResult :: Word64 -> GenParams -> IO (PropertyResult, Bool)
+          toResult it params =
+                    (propertyToResult <$> evaluate (runGen (unProp prop) (rngIt it) params))
+            `catch` (\(e :: SomeException) -> return (PropertyFailed (fromList $ show e), False))
 
     propertyToResult p =
         let args   = getArgs p
@@ -132,49 +261,3 @@ runProp ctx s prop = iterProp 1
 
     getChecks (PropertyArg _ p) = getChecks p
     getChecks (PropertyEOA c  ) = c
-
-    !rngIt  = genRng (contextSeed ctx) (s : contextGroups ctx)
-    !params = GenParams { genMaxSizeIntegral = 32   -- 256 bits maximum numbers
-                        , genMaxSizeArray    = 512  -- 512 elements
-                        , genMaxSizeString   = 8192 -- 8K string
-                        }
-
--- | Run tests
-defaultMain :: Test -> IO ()
-defaultMain test = do
-    -- parse arguments
-    --let arguments = [ "seed", "j" ]
-
-    -- generate a new seed
-    seed <- getRandomPrimType
-
-    let context = Context { contextLevel  = 0
-                          , contextGroups = []
-                          , contextSeed   = seed
-                          }
-
-    printHeader context
-    tr <- runTest context test
-    if nbFail tr > 0
-        then putStrLn (fromList (show $ nbFail tr) <> " failure(s)") >> exitFailure
-        else putStrLn "Success" >> exitSuccess
-  where
-    printHeader ctx = do
-        putStrLn ("seed: " <> fromList (show (contextSeed ctx))) -- TODO hexadecimal
-
-    runTest :: Context -> Test -> IO TestResult
-    runTest ctx (Group s l) = do
-        printIndent ctx s
-        results <- mapM (runTest (appendContext s ctx)) l
-        return $ GroupResult s (foldl' (+) 0 $ fmap nbFail results) results
-    runTest ctx (Property name prop) = do
-        (res, nbTests) <- runProp ctx name (property prop)
-        case res of
-            PropertySuccess  -> printIndent ctx $ "[  OK   ]   " <> name <> " (" <> fromList (show nbTests) <> " completed)"
-            PropertyFailed e -> printIndent ctx $ "[ ERROR ]   " <> name <> " after " <> fromList (show (nbTests-1)) <> " tests\n" <> e
-        return (PropertyResult name nbTests res)
-
-    runTest _ (Unit _ _) = do
-        error "not implemented"
-
-    printIndent ctx s = putStrLn (replicate (contextLevel ctx) ' ' <> s)
