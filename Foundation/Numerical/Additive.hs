a1dfa03c0f5f82e89e9bfb8a1fc5a6747d0f9f7b
Vincent Hanquez <vincent@snarc.org>
2017-05-24 20:51:54 +0100

    Add direct use of primitives operation in addition

--- Foundation/Numerical/Additive.hs
+++ Foundation/Numerical/Additive.hs
@@ -1,12 +1,24 @@
 {-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE MagicHash         #-}
 module Foundation.Numerical.Additive
     ( Additive(..)
     ) where
+
+#include "MachDeps.h"
 
 import           Foundation.Internal.Base
 import           Foundation.Internal.Natural
 import           Foundation.Numerical.Number
 import qualified Prelude
+import           GHC.Types
+import           GHC.Prim
+import           GHC.Int
+import           GHC.Word
+
+#if WORD_SIZE_IN_BITS < 64
+import           GHC.IntWord64
+#endif
 
 -- | Represent class of things that can be added together,
 -- contains a neutral element and is commutative.
@@ -35,27 +47,31 @@ instance Additive Integer where
     scale = scaleNum
 instance Additive Int where
     azero = 0
-    (+) = (Prelude.+)
+    (I# a) + (I# b) = I# (a +# b)
     scale = scaleNum
 instance Additive Int8 where
     azero = 0
-    (+) = (Prelude.+)
+    (I8# a) + (I8# b) = I8# (narrow8Int# (a +# b))
     scale = scaleNum
 instance Additive Int16 where
     azero = 0
-    (+) = (Prelude.+)
+    (I16# a) + (I16# b) = I16# (narrow16Int# (a +# b))
     scale = scaleNum
 instance Additive Int32 where
     azero = 0
-    (+) = (Prelude.+)
+    (I32# a) + (I32# b) = I32# (narrow32Int# (a +# b))
     scale = scaleNum
 instance Additive Int64 where
     azero = 0
-    (+) = (Prelude.+)
+#if WORD_SIZE_IN_BITS == 64
+    (I64# a) + (I64# b) = I64# (a +# b)
+#else
+    (I64# a) + (I64# b) = I64# (a `plusInt64#` b)
+#endif
     scale = scaleNum
 instance Additive Word where
     azero = 0
-    (+) = (Prelude.+)
+    (W# a) + (W# b) = W# (a `plusWord#` b)
     scale = scaleNum
 instance Additive Natural where
     azero = 0
@@ -63,27 +79,31 @@ instance Additive Natural where
     scale = scaleNum
 instance Additive Word8 where
     azero = 0
-    (+) = (Prelude.+)
+    (W8# a) + (W8# b) = W8# (narrow8Word# (a `plusWord#` b))
     scale = scaleNum
 instance Additive Word16 where
     azero = 0
-    (+) = (Prelude.+)
+    (W16# a) + (W16# b) = W16# (narrow16Word# (a `plusWord#` b))
     scale = scaleNum
 instance Additive Word32 where
     azero = 0
-    (+) = (Prelude.+)
+    (W32# a) + (W32# b) = W32# (narrow32Word# (a `plusWord#` b))
     scale = scaleNum
 instance Additive Word64 where
     azero = 0
-    (+) = (Prelude.+)
+#if WORD_SIZE_IN_BITS == 64
+    (W64# a) + (W64# b) = W64# (a `plusWord#` b)
+#else
+    (W64# a) + (W64# b) = W64# (int64ToWord64# (word64ToInt64# a `plusInt64#` word64ToInt64# b))
+#endif
     scale = scaleNum
 instance Additive Prelude.Float where
     azero = 0.0
-    (+) = (Prelude.+)
+    (F# a) + (F# b) = F# (a `plusFloat#` b)
     scale = scaleNum
 instance Additive Prelude.Double where
     azero = 0.0
-    (+) = (Prelude.+)
+    (D# a) + (D# b) = D# (a +## b)
     scale = scaleNum
 
 scaleNum :: (Prelude.Num a, IsNatural n) => n -> a -> a
