bec6c887f0ffb17d17d32fb52e0ef0a1e223d02a
Vincent Hanquez <vincent@snarc.org>
2017-05-27 15:20:38 +0100

    Tweak bindings of memcmp to have a version for all possible combinaison

--- Foundation/System/Bindings/Hs.hs
+++ Foundation/System/Bindings/Hs.hs
@@ -5,11 +5,20 @@ module Foundation.System.Bindings.Hs
     where
 
 import GHC.IO
-import GHC.Types
 import GHC.Prim
 import Foreign.C.Types
+import Foreign.Ptr
 
 foreign import ccall unsafe "HsBase.h __hscore_get_errno" sysHsCoreGetErrno :: IO CInt
 
-foreign import ccall unsafe "_foundation_memcmp" sysHsMemcmp ::
+foreign import ccall unsafe "_foundation_memcmp" sysHsMemcmpBaBa ::
     ByteArray# -> CSize -> ByteArray# -> CSize -> CSize -> IO CInt
+
+foreign import ccall unsafe "_foundation_memcmp" sysHsMemcmpBaPtr ::
+    ByteArray# -> CSize -> Ptr a -> CSize -> CSize -> IO CInt
+
+foreign import ccall unsafe "_foundation_memcmp" sysHsMemcmpPtrBa ::
+    Ptr a -> CSize -> ByteArray# -> CSize -> CSize -> IO CInt
+
+foreign import ccall unsafe "_foundation_memcmp" sysHsMemcmpPtrPtr ::
+    Ptr a -> CSize -> Ptr b -> CSize -> CSize -> IO CInt
