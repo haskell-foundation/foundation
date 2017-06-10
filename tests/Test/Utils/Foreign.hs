71773ddb91abad846e65ad3ae3310f7daf91afb8
Galder Zamarreño <galder@zamarreno.com>
2017-06-10 10:23:34 +0200

    HLint: Use uncurry

--- tests/Test/Utils/Foreign.hs
+++ tests/Test/Utils/Foreign.hs
@@ -24,5 +24,5 @@ createPtr l
         let (CountOf szElem) = size (Proxy :: Proxy e)
             nbBytes = szElem * (let (CountOf c) = length l in c)
         ptr <- mallocBytes nbBytes
-        forM_ (zip [0..] l) $ \(o, e) -> pokeOff ptr o e
+        forM_ (zip [0..] l) $ uncurry (pokeOff ptr)
         toFinalPtr ptr free
