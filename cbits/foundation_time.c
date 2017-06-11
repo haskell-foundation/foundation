7075400201089d1321e9af9ca883c7e536c59ca5
Nicolas DI PRIMA <nicolas@primetype.co.uk>
2017-06-11 20:10:44 +0200

    baby steps toward using time on windows too
    relates to #326

--- cbits/foundation_time.c
+++ cbits/foundation_time.c
@@ -1,5 +1,9 @@
 #include "foundation_system.h"
 
+#if FOUNDATION_SYSTEM_WINDOWS
+#include "Windows.h"
+#endif
+
 #ifdef FOUNDATION_SYSTEM_API_NO_CLOCK
 
 #ifdef FOUNDATION_SYSTEM_MACOS
@@ -81,4 +85,44 @@ int foundation_time_clock_gettime(unsigned int clockid, struct timespec *timespe
 
 #endif
 
+#endif
+
+#if FOUNDATION_SYSTEM_WINDOWS
+int clock_gettime(int X, struct timeval *tv)
+{
+    LARGE_INTEGER           t;
+    FILETIME            f;
+    double                  microseconds;
+    static LARGE_INTEGER    offset;
+    static double           frequencyToMicroseconds;
+    static int              initialized = 0;
+    static BOOL             usePerformanceCounter = 0;
+
+    if (!initialized) {
+        LARGE_INTEGER performanceFrequency;
+        initialized = 1;
+        usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
+        if (usePerformanceCounter) {
+            QueryPerformanceCounter(&offset);
+            frequencyToMicroseconds = (double)performanceFrequency.QuadPart / 1000000.;
+        } else {
+            offset = getFILETIMEoffset();
+            frequencyToMicroseconds = 10.;
+        }
+    }
+    if (usePerformanceCounter) QueryPerformanceCounter(&t);
+    else {
+        GetSystemTimeAsFileTime(&f);
+        t.QuadPart = f.dwHighDateTime;
+        t.QuadPart <<= 32;
+        t.QuadPart |= f.dwLowDateTime;
+    }
+
+    t.QuadPart -= offset.QuadPart;
+    microseconds = (double)t.QuadPart / frequencyToMicroseconds;
+    t.QuadPart = microseconds;
+    tv->tv_sec = t.QuadPart / 1000000;
+    tv->tv_usec = t.QuadPart % 1000000;
+    return (0);
+}
 #endif
