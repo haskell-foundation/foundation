e45c28a636730c61c293e26d2cd77e1f7fce7fdc
Alain Lehmann <alain.lehmann@gmail.com>
2017-08-20 21:53:56 +0200

    quicksort: adding benchmark for sorting of UArray
    benchmark results for existing sortBy
    
    random: mean 18.71 ms (18.47 ms .. 18.97 ms)
    sorted: mean 3.349 s (3.342 s .. 3.353 s)
    reverse: mean 898.3 ms (889.3 ms .. 903.8 ms)
    cyclic: mean 35.61 ms (35.34 ms .. 35.91 ms)

--- benchs/Array.hs
+++ benchs/Array.hs
@@ -1,16 +1,26 @@
 module Main (main) where
 
 import Foundation
+import Foundation.Random
 import Foundation.Collection as F
 import Criterion.Main
+import qualified Prelude as P
 
-main = defaultMain
-    [ bgroup "Uarray"
+main = do
+    rndInput <- getRandomBytes 10000
+    defaultMain [ bgroup "Uarray"
         [ bench "fromList [Word8]" $ whnf (fromList :: [Word8] -> UArray Word8) [1..255]
         , bench "fromList [Word16]" $ whnf (fromList :: [Word16] -> UArray Word16) [1..1024]
         , bench "break" $ whnf (F.break (== 255)) input
+        , bench "sort random"  $ whnf sort rndInput
+        , bench "sort sorted"  $ whnf sort (sort rndInput)
+        , bench "sort reverse" $ whnf sort (reverse.sort $ rndInput)
+        , bench "sort cyclic"  $ whnf sort inputLong
         ]
-    ]
+      ]
   where
-    input :: UArray Word8
+    input, inputLong :: UArray Word8
     input = fromList ([1..255] <> [1..255])
+    inputLong = fromList . P.take 10000 . P.cycle $ [1..255]
+
+    sort = F.sortBy compare
