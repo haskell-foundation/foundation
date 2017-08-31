e37c0689fe3b9ca4cac27a0b7464ba6cfa9bb55c
Vincent Hanquez <vincent@snarc.org>
2017-08-31 07:37:14 +0100

    Add many instances for From/TryFrom

--- basement/Basement/From.hs
+++ basement/Basement/From.hs
@@ -2,6 +2,24 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE MagicHash             #-}
+-- |
+-- Module      : Basement.From
+-- License     : BSD-style
+-- Maintainer  : Haskell Foundation
+--
+-- Flexible Type convertion
+--
+-- From is multi parameter type class that allow converting
+-- from a to b.
+--
+-- Only type that are valid to convert to another type
+-- should be From instance; otherwise TryFrom should be used.
+--
+-- Into (resp TryInto) allows the contrary instances to be able
+-- to specify the destination type before the source. This is
+-- practical with TypeApplication
 module Basement.From
     ( From(..)
     , Into
@@ -12,9 +30,30 @@ module Basement.From
     ) where
 
 import Basement.Compat.Base
-import Basement.IntegralConv
 
--- | Class of things that can be converted from a to b
+-- basic instances
+import           GHC.Prim
+import           GHC.Int
+import           GHC.Word
+import qualified Basement.Block as Block
+import qualified Basement.BoxedArray as BoxArray
+import qualified Basement.UArray as UArray
+import qualified Basement.String as String
+import qualified Basement.Types.AsciiString as AsciiString
+import           Basement.These
+import           Basement.PrimType (PrimType)
+import           Basement.Types.OffsetSize
+
+-- nat instances
+#if __GLASGOW_HASKELL__ >= 800
+import           Basement.Nat
+import qualified Basement.BlockN as BlockN
+#endif
+
+-- | Class of things that can be converted from a to b.
+--
+-- In a valid instance, the source should be always representable by the destination,
+-- otherwise the instance should be using 'TryFrom'
 class From a b where
     from :: a -> b
 
@@ -44,7 +83,74 @@ tryInto = tryFrom
 instance From a a where
     from = id
 
+-- Simple numerical instances
 instance From Int Word where
-    from = integralCast
+    from (I# i) = W# (int2Word# i)
 instance From Word Int where
-    from = integralCast
+    from (W# w) = I# (word2Int# w)
+
+-- Simple prelude types
+instance From (Maybe a) (Either () a) where
+    from (Just x) = Right x
+    from Nothing  = Left ()
+
+-- basic basement types
+instance From (CountOf ty) Int where
+    from (CountOf n) = n
+instance From (CountOf ty) Word where
+    from (CountOf n) = from n
+
+instance From (Either a b) (These a b) where
+    from (Left a) = This a
+    from (Right b) = That b
+
+-- basement instances
+
+-- uarrays
+instance PrimType ty => From (Block.Block ty) (UArray.UArray ty) where
+    from = UArray.fromBlock
+instance PrimType ty => From (BoxArray.Array ty) (UArray.UArray ty) where
+    from = BoxArray.mapToUnboxed id
+
+-- blocks
+instance PrimType ty => From (UArray.UArray ty) (Block.Block ty) where
+    from = UArray.toBlock
+instance PrimType ty => From (BoxArray.Array ty) (Block.Block ty) where
+    from = UArray.toBlock . BoxArray.mapToUnboxed id
+
+-- boxed array
+instance PrimType ty => From (UArray.UArray ty) (BoxArray.Array ty) where
+    from = BoxArray.mapFromUnboxed id
+
+
+instance From String.String (UArray.UArray Word8) where
+    from = String.toBytes String.UTF8
+
+instance From AsciiString.AsciiString String.String where
+    from = String.fromBytesUnsafe . UArray.unsafeRecast . AsciiString.toBytes
+instance From AsciiString.AsciiString (UArray.UArray Word8) where
+    from = UArray.unsafeRecast . AsciiString.toBytes
+
+instance TryFrom (UArray.UArray Word8) String.String where
+    tryFrom arr = case String.fromBytes String.UTF8 arr of
+                    (s, Nothing, _) -> Just s
+                    (_, Just _, _)  -> Nothing
+
+#if __GLASGOW_HASKELL__ >= 800
+instance From (BlockN.BlockN n ty) (Block.Block ty) where
+    from = BlockN.toBlock
+instance (NatWithinBound Int n, PrimType ty) => From (BlockN.BlockN n ty) (UArray.UArray ty) where
+    from = UArray.fromBlock . BlockN.toBlock
+instance (NatWithinBound Int n, PrimType ty) => From (BlockN.BlockN n ty) (BoxArray.Array ty) where
+    from = BoxArray.mapFromUnboxed id . UArray.fromBlock . BlockN.toBlock
+
+instance (NatWithinBound (CountOf ty) n, KnownNat n, PrimType ty)
+      => TryFrom (Block.Block ty) (BlockN.BlockN n ty) where
+    tryFrom = BlockN.toBlockN
+instance (NatWithinBound (CountOf ty) n, KnownNat n, PrimType ty)
+      => TryFrom (UArray.UArray ty) (BlockN.BlockN n ty) where
+    tryFrom = BlockN.toBlockN . UArray.toBlock
+instance (NatWithinBound (CountOf ty) n, KnownNat n, PrimType ty)
+      => TryFrom (BoxArray.Array ty) (BlockN.BlockN n ty) where
+    tryFrom = BlockN.toBlockN . UArray.toBlock . BoxArray.mapToUnboxed id
+#endif
