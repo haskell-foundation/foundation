98f82ca51f438662a9b2b93c5541fbec9a81edfc
laptop1\Andrew <andrewjpritchard@gmail.com>
2017-11-04 17:33:03 +0800

    Tested on windows (again)

--- basement/Basement/Terminal.hs
+++ basement/Basement/Terminal.hs
@@ -9,19 +9,19 @@ import Basement.Compat.Base
 import Foreign.C
 import qualified Prelude as P
 
+#ifdef mingw32_HOST_OS
+import System.IO (hSetEncoding, utf8, hPutStrLn, stderr, stdin, stdout)
+import System.Win32.Console (setConsoleCP, setConsoleOutputCP, getConsoleCP, getConsoleOutputCP)
+#endif
+
 foreign import ccall "get_terminal_width" get_terminal_width :: IO CInt
-foreign import ccall "get_terminal_height" get_terminal_height :: IO CInt
+foreign import ccall "get_terminal_height" get_terminal_height :: IO CInt      
 
 size :: IO (Int, Int)
 size = do
     width <- get_terminal_width
     height <- get_terminal_height
     return (P.fromIntegral width, P.fromIntegral height)
-
-#ifdef mingw32_HOST_OS
-import System.IO (hSetEncoding, utf8, hPutStrLn, stderr, stdin, stdout)
-import System.Win32.Console (setConsoleCP, setConsoleOutputCP, getConsoleCP, getConsoleOutputCP)
-#endif
 
 initialize :: IO ()
 initialize = do
--- basement/cbits/foundation_terminal.c
+++ basement/cbits/foundation_terminal.c
@@ -3,31 +3,62 @@
 #ifdef FOUNDATION_SYSTEM_WINDOWS
 #include <windows.h>
 
-#else
+#elif defined FOUNDATION_SYSTEM_UNIX
 #include <sys/ioctl.h>
 
 #endif
 
+#include <stdlib.h>
+#include <stdio.h>
+
 int get_terminal_width (void) {
 #ifdef FOUNDATION_SYSTEM_WINDOWS
-    CONSOLE_SCREEN_BUFFER_INFO max;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &max);
-    return max.srWindow.Right - max.srWindow.Left + 1;
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hStdout != INVALID_HANDLE_VALUE) {
+        CONSOLE_SCREEN_BUFFER_INFO max;
+        if (0 != GetConsoleScreenBufferInfo(hStdout, &max)) {
+            return max.srWindow.Right - max.srWindow.Left + 1;
+        }
+    }
 #elif defined FOUNDATION_SYSTEM_UNIX
     struct winsize max;
-    ioctl(0, TIOCGWINSZ , &max);
-    return max.ws_col;
+    if (-1 != ioctl(0, TIOCGWINSZ , &max)) {
+        return max.ws_col;
+    }
 #endif
+    FILE* fp = popen ("tput cols", "r");
+    if (fp != NULL) {
+        int result;
+        if (1 == fscanf (fp, " %d ", &result)) {
+            return result;
+        }
+    }
+    pclose (fp);
+    return 80;
 };
 
 int get_terminal_height (void) {
 #ifdef FOUNDATION_SYSTEM_WINDOWS
-    CONSOLE_SCREEN_BUFFER_INFO max;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &max);
-    return max.srWindow.Bottom - max.srWindow.Top + 1;
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hStdout != INVALID_HANDLE_VALUE) {
+        CONSOLE_SCREEN_BUFFER_INFO max;
+        if (0 != GetConsoleScreenBufferInfo(hStdout, &max)) {
+            return max.srWindow.Bottom - max.srWindow.Top + 1;
+        }
+    }
 #elif defined FOUNDATION_SYSTEM_UNIX
     struct winsize max;
-    ioctl(0, TIOCGWINSZ , &max);
-    return max.ws_row;
+    if (-1 != ioctl(0, TIOCGWINSZ , &max)) {
+        return max.ws_row;
+    }
 #endif
+    FILE* fp = popen ("tput lines", "r");
+    if (fp != NULL) {
+        int result;
+        if (1 == fscanf (fp, " %d ", &result)) {
+            return result;
+        }
+    }
+    pclose (fp);
+    return 25;
 }
