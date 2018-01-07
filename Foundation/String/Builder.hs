2e5eb8904de8616c5d1733cae80b629054871317
Nicolas Di Prima <nicolas@primetype.co.uk>
2018-01-07 20:17:05 +0000

    add compatibility function (toString) and remove old comment

--- Foundation/String/Builder.hs
+++ Foundation/String/Builder.hs
@@ -5,12 +5,20 @@
 --
 -- String Builder
 --
--- This is extremely bad implementation of a builder implementation
--- but provide a very similar API to the future fast implementation;
--- So, in the spirit of getting started and to be able to start using
--- the API, we don't wait for the fast implementation.
 module Foundation.String.Builder
     ( module Basement.String.Builder
+    , toString
     ) where
 
 import Basement.String.Builder
+import Basement.String (String)
+import GHC.ST
+
+-- | run the builder and return a `String`
+--
+-- alias to `runUnsafe`
+--
+-- This function is not safe, prefer `run`.
+--
+toString :: Builder -> String
+toString builder = runST (runUnsafe builder)
