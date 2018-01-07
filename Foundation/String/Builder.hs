17d43ceff954180d11884ea3497d0cc6397047bc
Nicolas Di Prima <nicolas@primetype.co.uk>
2018-01-07 20:11:59 +0000

    replace legacy foundation's builder by the basement's one

--- Foundation/String/Builder.hs
+++ Foundation/String/Builder.hs
@@ -10,46 +10,7 @@
 -- So, in the spirit of getting started and to be able to start using
 -- the API, we don't wait for the fast implementation.
 module Foundation.String.Builder
-    ( Builder
-    , emit
-    , emitChar
-    , toString
+    ( module Basement.String.Builder
     ) where
 
-import           Basement.Compat.Base
-import           Basement.Compat.Semigroup
-import           Basement.String                (String)
-import qualified Basement.String as S
-
-data Builder = E String | T [Builder]
-
-instance IsString Builder where
-    fromString = E . fromString
-
-instance Semigroup Builder where
-    (<>) = append
-instance Monoid Builder where
-    mempty = empty
-    mappend = append
-    mconcat = concat
-
-empty :: Builder
-empty = T []
-
-emit :: String -> Builder
-emit s = E s
-
-emitChar :: Char -> Builder
-emitChar c = E (S.singleton c)
-
-toString :: Builder -> String
-toString = mconcat . flatten
-  where
-    flatten (E s) = [s]
-    flatten (T l) = mconcat $ fmap flatten l
-
-append :: Builder -> Builder -> Builder
-append a b = T [a,b]
-
-concat :: [Builder] -> Builder
-concat = T
+import Basement.String.Builder
