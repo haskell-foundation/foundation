77b1c6fd9c22f7e9a007c052dafab5442c3e67e9
Nicolas Di Prima <nicolas@primetype.co.uk>
2018-02-09 11:57:35 +0000

    add Copy instances for BlockN and ListN

--- Foundation/Collection/Copy.hs
+++ Foundation/Collection/Copy.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE UndecidableInstances #-}
+
 module Foundation.Collection.Copy
     ( Copy(..)
     ) where
 
 import           GHC.ST (runST)
 import           Basement.Compat.Base ((>>=))
+import           Basement.Nat
+import           Basement.Types.OffsetSize
 import qualified Basement.Block as BLK
+import qualified Basement.Sized.Block as BLKN
+import qualified Basement.Sized.List  as LN
 import qualified Basement.UArray as UA
 import qualified Basement.BoxedArray as BA
 import qualified Basement.String as S
@@ -13,8 +19,12 @@ class Copy a where
     copy :: a -> a
 instance Copy [ty] where
     copy a = a
+instance Copy (LN.ListN n ty) where
+    copy a = a
 instance UA.PrimType ty => Copy (BLK.Block ty) where
     copy blk = runST (BLK.thaw blk >>= BLK.unsafeFreeze)
+instance (Countable ty n, UA.PrimType ty, KnownNat n) => Copy (BLKN.BlockN n ty) where
+    copy blk = runST (BLKN.thaw blk >>= BLKN.freeze)
 instance UA.PrimType ty => Copy (UA.UArray ty) where
     copy = UA.copy
 instance Copy (BA.Array ty) where
