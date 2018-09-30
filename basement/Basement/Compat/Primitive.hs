2a78d3ffacd52288d49d9af761b8b9371efe0f62
Vincent Hanquez <vincent@typed.io>
2018-09-30 18:28:11 +0100

    remove compat #2

--- basement/Basement/Compat/Primitive.hs
+++ basement/Basement/Compat/Primitive.hs
@@ -12,7 +12,6 @@
 module Basement.Compat.Primitive
     ( bool#
     , PinnedStatus(..), toPinnedStatus#
-    , compatAndI#
     , compatQuotRemInt#
     , compatCopyAddrToByteArray#
     , compatCopyByteArrayToAddr#
@@ -33,11 +32,16 @@ import           GHC.IO
 
 import           Basement.Compat.PrimTypes
 
+--  GHC 8.4  | Base 4.11
+--  GHC 8.2  | Base 4.10
 --  GHC 8.0  | Base 4.9
 --  GHC 7.10 | Base 4.8
 --  GHC 7.8  | Base 4.7
 --  GHC 7.6  | Base 4.6
 --  GHC 7.4  | Base 4.5
+--
+--  More complete list:
+--  https://wiki.haskell.org/Base_package
 
 -- | Flag record whether a specific byte array is pinned or not
 data PinnedStatus = Pinned | Unpinned
@@ -52,15 +56,6 @@ bool# :: Int# -> Prelude.Bool
 bool# v = isTrue# v
 {-# INLINE bool# #-}
 
--- | A version friendly of andI#
-compatAndI# :: Int# -> Int# -> Int#
-#if !MIN_VERSION_base(4,7,0)
-compatAndI# a b = word2Int# (and# (int2Word# a) (int2Word# b))
-#else
-compatAndI# = andI#
-#endif
-{-# INLINE compatAndI# #-}
-
 -- | A version friendly of quotRemInt#
 compatQuotRemInt# :: Int# -> Int# -> (# Int#, Int# #)
 compatQuotRemInt# = quotRemInt#
@@ -70,35 +65,14 @@ compatQuotRemInt# = quotRemInt#
 --
 -- only available from GHC 7.8
 compatCopyAddrToByteArray# :: Addr# -> MutableByteArray# s -> Int# -> Int# -> State# s -> State# s
-#if MIN_VERSION_base(4,7,0)
 compatCopyAddrToByteArray# = copyAddrToByteArray#
-#else
-compatCopyAddrToByteArray# addr ba ofs sz stini =
-    loop ofs 0# stini
-  where
-    loop o i st
-        | bool# (i ==# sz)  = st
-        | Prelude.otherwise =
-            case readWord8OffAddr# addr i st of
-                (# st2, w #) -> loop (o +# 1#) (i +# 1#) (writeWord8Array# ba o w st2)
-#endif
 {-# INLINE compatCopyAddrToByteArray# #-}
 
 -- | A version friendly fo copyByteArrayToAddr#
 --
 -- only available from GHC 7.8
 compatCopyByteArrayToAddr# :: ByteArray# -> Int# -> Addr# -> Int# -> State# s -> State# s
-#if MIN_VERSION_base(4,7,0)
 compatCopyByteArrayToAddr# = copyByteArrayToAddr#
-#else
-compatCopyByteArrayToAddr# ba ofs addr sz stini =
-    loop ofs 0# stini
-  where
-    loop o i st
-        | bool# (i ==# sz)  = st
-        | Prelude.otherwise =
-            loop (o +# 1#) (i +# 1#) (writeWord8OffAddr# addr i (indexWord8Array# ba o) st)
-#endif
 {-# INLINE compatCopyByteArrayToAddr# #-}
 
 -- | A mkWeak# version that keep working on 8.0
@@ -108,48 +82,21 @@ compatCopyByteArrayToAddr# ba ofs addr sz stini =
 -- * 0.5 :mkWeak# :: o -> b -> (State# RealWorld -> (#State# RealWorld, c#)) -> State# RealWorld -> (#State# RealWorld, Weak# b#)
 --
 compatMkWeak# :: o -> b -> Prelude.IO () -> State# RealWorld -> (#State# RealWorld, Weak# b #)
-#if __GLASGOW_HASKELL__ >= 800
 compatMkWeak# o b c s = mkWeak# o b (case c of { IO f -> f }) s
-#else
-compatMkWeak# o b c s = mkWeak# o b c s
-#endif
 {-# INLINE compatMkWeak# #-}
 
 compatGetSizeofMutableByteArray# :: MutableByteArray# s -> State# s -> (#State# s, Int# #)
-#if __GLASGOW_HASKELL__ >= 800
 compatGetSizeofMutableByteArray# mba s = getSizeofMutableByteArray# mba s
-#else
-compatGetSizeofMutableByteArray# mba s = (# s, sizeofMutableByteArray# mba #)
-#endif
 {-# INLINE compatGetSizeofMutableByteArray# #-}
 
 compatShrinkMutableByteArray# :: MutableByteArray# s -> Int# -> State# s -> (# State# s, MutableByteArray# s #)
-#if __GLASGOW_HASKELL__ >= 800
 compatShrinkMutableByteArray# mba i s =
     case shrinkMutableByteArray# mba i s of { s2 -> (# s2, mba #) }
-#else
-compatShrinkMutableByteArray# src i s =
-    -- not check whether i is smaller than the size of the buffer
-    case newAlignedPinnedByteArray# i 8# s of { (# s2, dst #) ->
-    case copyMutableByteArray# src 0# dst 0# i s2 of { s3 -> (# s3, dst #) }}
-#endif
 {-# INLINE compatShrinkMutableByteArray# #-}
 
 --shrinkMutableByteArray# :: MutableByteArray# s -> Int# -> State# s -> State# s
 compatResizeMutableByteArray# :: MutableByteArray# s -> Int# -> State# s -> (# State# s, MutableByteArray# s #)
-#if __GLASGOW_HASKELL__ >= 800
 compatResizeMutableByteArray# mba i s = resizeMutableByteArray# mba i s
-#else
-compatResizeMutableByteArray# src i s =
-    case newAlignedPinnedByteArray# i 8# s of { (# s2, dst #) ->
-    case copyMutableByteArray# src 0# dst 0# nbBytes s2 of { s3 -> (# s3, dst #) }}
-  where
-    isGrow = bool# (i ># len)
-    nbBytes
-        | isGrow            = len
-        | Prelude.otherwise = i
-    !len = sizeofMutableByteArray# src
-#endif
 {-# INLINE compatResizeMutableByteArray# #-}
 
 #if __GLASGOW_HASKELL__ >= 802
